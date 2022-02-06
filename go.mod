09ef4792110f35a1d24f51cd5882aa56d3806ab8
Andrew Snodgrass <git@pointlander.info>
2022-02-05 19:58:58 -0600

    Reconstruct from eigen vector

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/pointlander/truther
 go 1.16
 
 require (
+	github.com/pointlander/gradient v0.0.0-20201206051041-dbff480e6d28 // indirect
 	gonum.org/v1/gonum v0.9.3 // indirect
 	gonum.org/v1/plot v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,9 +6,17 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/cmplx"
+	"math/rand"
 
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+
+	"github.com/pointlander/gradient/tc128"
 )
 
 const (
@@ -17,6 +25,8 @@ const (
 )
 
 func main() {
+	rand.Seed(1)
+
 	data := []float64{
 		0, 1, 0, 1, 1,
 		1, 0, 1, 0, 1,
@@ -50,6 +60,89 @@ func main() {
 	for i := 0; i < Size; i++ {
 		for j := 0; j < Size; j++ {
 			fmt.Printf("(%f, %f) ", cmplx.Abs(vectors.At(i, j)), cmplx.Phase(vectors.At(i, j)))
+		}
+		fmt.Printf("\n")
+	}
+
+	random128 := func(a, b float64) complex128 {
+		return complex((b-a)*rand.Float64()+a, (b-a)*rand.Float64()+a)
+	}
+
+	set := tc128.NewSet()
+	set.Add("A", Size, Size)
+	set.Add("X", Size, 1)
+	set.Add("Y", Size, 1)
+
+	w := set.Weights[0]
+	for i := 0; i < cap(w.X); i++ {
+		w.X = append(w.X, random128(-1, 1))
+	}
+
+	w = set.Weights[1]
+	for i := 0; i < Size; i++ {
+		w.X = append(w.X, vectors.At(0, i))
+	}
+
+	w = set.Weights[2]
+	for i := 0; i < Size; i++ {
+		w.X = append(w.X, values[0]*vectors.At(0, i))
+	}
+
+	l1 := tc128.Mul(set.Get("A"), set.Get("X"))
+	cost := tc128.Quadratic(set.Get("Y"), l1)
+
+	eta, iterations := complex128(.3), 128
+	points := make(plotter.XYs, 0, iterations)
+	i := 0
+	for i < iterations {
+		set.Zero()
+
+		total := tc128.Gradient(cost).X[0]
+		sum := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				sum += cmplx.Abs(d) * cmplx.Abs(d)
+			}
+		}
+		norm := float64(math.Sqrt(float64(sum)))
+		scaling := float64(1)
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+
+		w := set.Weights[0]
+		for l, d := range w.D {
+			w.X[l] -= eta * d * complex(scaling, 0)
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: float64(cmplx.Abs(total))})
+		fmt.Println(i, cmplx.Abs(total))
+		i++
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "cost.png")
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < Size; i++ {
+		for j := 0; j < Size; j++ {
+			value := set.Weights[0].X[i*Size+j]
+			fmt.Printf("%f ", cmplx.Abs(value))
 		}
 		fmt.Printf("\n")
 	}
